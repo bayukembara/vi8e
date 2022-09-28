df9d69bbcbc49721a335dc6424d8fe56b54272bf
Bayu K <bayukembara@gmail.com>
2022-09-29 01:16:04 +0700

    Front-End PC Done

---
+++ index.js
@@ -0,0 +1,10 @@
+var swiper = new Swiper(".swiper", {
+  slidesPerView: 5,
+  spaceBetween: -30,
+  slidesPerGroup: 3,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
